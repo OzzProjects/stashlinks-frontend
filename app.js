dbcd8286ad4c3fef3614aef796f3e823461926c5
Osezele <osezele06@gmail.com>
2022-01-04 11:03:10 +0000

    change from client to pool

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const { Client } = require('pg');
-const client = new Client({
+const { Client, Pool } = require('pg');
+const pool = new Pool({
   connectionString: process.env.STASH_DB_FRONTEND_URL,
   ssl: {
     rejectUnauthorized: false
@@ -53,37 +53,39 @@ function getDataDatabase(callback){
 
   var response="";
 
-  client.connect(err => {
+  // the pool will emit an error on behalf of any idle clients
+  // it contains if a backend error or network partition happens
+  pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+  })
+
+  const text = 'SELECT link FROM links'
+ 
+  // callback - checkout a client
+  pool.connect((err, client, done) => {
+
     if (err) {
       console.error('connection error', err.stack)
     } else {
       console.log('connected')
     }
-  });
 
-  const text = 'SELECT link FROM links'
-
-  client.query(text, (err, res) => {
-    if (err) {
-      console.log("Failed:")
-      console.log(err.stack)
-      response= null;
-      callback(null);
-    } else {
-      console.log("Succeded:")
-      console.log(res.rows[0])    
-      response=res.rows[0];
-      callback(res.rows[0]);
-    }
-
-    client.end(err => {
-      console.log('client has disconnected')
+    client.query(text, (err, res) => {
+      done()
       if (err) {
-        console.log('error during disconnection', err.stack)
+        console.log("Failed:")
+        console.log(err.stack)
+        callback(null);
+      } else {
+        console.log("Succeded:")
+        console.log(res.rows[0])
+        callback(res.rows[0]);
       }
     })
-  })
+  });
 
+  pool.end();
 
   console.log("Final response:"+response);
   return response;
