9e5b2461bcea309fe321ceb51d6bac2302bd6e71
Osezele <osezele06@gmail.com>
2022-01-04 12:30:04 +0000

    refactor - set global inside the callback

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/script',function(req,res,next){
 
   console.log('Request message: ' + req.body);
 
-  var response= getDataDatabase();
+  var response= getDataDatabase(returnData);
   console.log("returned response: "+response);
   if(response === ""|| response == null){
     res.status(404).send({data:'Database error'});
@@ -44,8 +44,19 @@ app.get('/script',function(req,res,next){
 
 
 var dataResponse="";
+function returnData(data){
+  if(data!=null){
+    console.log("The data callback "+data);
+    dataResponse=data;
+    return data;
+  }
+  console.log("The error callback "+data);
+  dataResponse=null;
+  return null;
 
-function getDataDatabase(){
+}
+
+function getDataDatabase(callback){
   // the pool will emit an error on behalf of any idle clients it contains if a backend error or network partition happens
   pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err)
@@ -54,20 +65,24 @@ function getDataDatabase(){
   const text = 'SELECT link FROM links'
   pool.connect((err, client, done) => {
 
-    if (err) { console.error('connection error', err.stack)  } else {    console.log('connected') }
+    if (err) { 
+      console.error('connection error', err.stack)  
+    } else {    
+      console.log('connected') 
+    }
 
     client.query(text, (err, res) => {
      
       if (err) {
         console.log("Failed:")
         console.log(err.stack)
-        dataResponse=null;
+        callback(null);
         done()
         return;
       } else {
         console.log("Succeded:")
         console.log(res.rows[0])
-        dataResponse=res.rows[0];
+        callback(res.rows[0]);
         done()
         return;
       }
